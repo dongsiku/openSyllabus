8735413b3e4ffc14b16366bfa12b69c4554da9b4
sik103 <dongsiku+github@gmail.com>
2018-04-05 21:03:03 +0900

    Make js show Error Messege

--- assets/js/openOkadaiSyllabus.js
+++ assets/js/openOkadaiSyllabus.js
@@ -1,17 +1,22 @@
 function openOkadaiSyllabus(num1){
+    document.getElementById("errmsg").innerHTML = '';
 
     if(num1 == 1){
         var lang = "ja_JP";
     }else if(num1 == 2){
         var lang = "en_US";
     }else{
-        window.open("index.html");
-        console.log("debug mode");
+        console.log("debug mode")
         return false;
     }
 
 
-    var hoge = document.getElementById("inputClassNo").value;
+    var classno = document.getElementById("inputClassNo").value;
+    if(classno.length < 6 || isNaN(classno)){
+        console.log("ValueError: classno");
+        document.getElementById("errmsg").innerHTML = '<strong>ValueError</strong>';
+        return false;
+    }
     
     var dt = new Date();
     var month = dt.getMonth();
@@ -22,9 +27,9 @@ function openOkadaiSyllabus(num1){
     var url = "https://gs.okayama-u.ac.jp/campusweb/campussquare.do?_flowId=SYW4101101-flow&nendo=";
     url += year;
     url += "&shozoku=";
-    url += hoge.substr(0,2);
+    url += classno.substr(0,2);
     url += "&jikanwari=";
-    url += hoge.substr(2,4);
+    url += classno.substr(2,4);
     // url += "&sylocale=ja_JP";
     url += "&sylocale=";
     url += lang;
