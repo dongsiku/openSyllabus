3ea295dad0676a2f9bdc63e8fdfa05e5434f1c3a
Dongsik <dongsiku+git@gmail.com>
2018-09-08 18:07:58 +0900

    Add feature to focus on the form

--- assets/js/openOkadaiSyllabus.js
+++ assets/js/openOkadaiSyllabus.js
@@ -21,6 +21,7 @@ function openOkadaiSyllabus(num1){
     console.log(url);
     window.open(url);
     document.classno-form.reset()
+    document.getElementById("inputClassNo").focus()
     return false;
 }
 
@@ -53,11 +54,11 @@ function make_url(classno, lang_mode){
     var dt = new Date();
     var month = dt.getMonth();
     var year = dt.getFullYear();
-    
+
     if(month < 1){
         year -= 1;
     }
-    
+
     var url = "https://gs.okayama-u.ac.jp/campusweb/campussquare.do?_flowId=SYW4101101-flow&nendo=";
     url += year;
     url += "&shozoku=";
